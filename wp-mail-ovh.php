77afc4f2f2dabdad4c798e9830d39b149a3262b9
hotfix31 <fred@lebarzic.fr>
2012-03-23 19:29:44 +0100

    Init

---
+++ wp-mail-ovh.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: WP Mail Manager OVH
+Plugin URI: http://www.lebarzic.fr/wp-mail-ovh
+Description: Outils intégrés a WordPress pour gérer les comptes mails de OVH
+Author: Frédéric Le Barzic
+Version: 0.1alpha
+Author URI: http://www.lebarzic.fr/
+*/
